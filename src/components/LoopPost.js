7c63e3105f204340ba4d81f644f314e84ed4fe65
yokokida <a.pirlo1109@gmail.com>
2023-12-04 23:14:32 +0900

    Add: loop post items

--- src/components/LoopPost.js
+++ src/components/LoopPost.js
@@ -1,4 +1,6 @@
-export default function LoopPost(props) {
+import Link from "next/link";
+
+function LoopPost(props) {
   return (
     <>
       <div class={`item-box ${props.id}`}>
@@ -22,3 +24,96 @@ export default function LoopPost(props) {
     </>
   );
 }
+
+function LoopWorks(props) {
+  return (
+    <>
+      <Link href={`/blog/${props.id}`} class={`item-box ${props.id}`}>
+        <div class="post-thumb">
+          <div class="img-box">
+            <img
+              src={props.thumbnail}
+              alt={props.title}
+              class="fadein-trigger"
+              width="920"
+              height="476"
+            />
+          </div>
+        </div>
+        <div class="post-info">
+          <div class="post-meta">
+            <time datetime={props.publishedAt} class="post-date">
+              {new Date(props.publishedAt)
+                .toLocaleDateString("ja-JP")
+                .split("/")
+                .join(".")}
+            </time>
+            <div class="post-cats">
+              <span class="post-cat">{props.categoryName}</span>
+            </div>
+          </div>
+          <h3 class="post-ttl">{props.title}</h3>
+        </div>
+      </Link>
+    </>
+  );
+}
+
+function LoopEvent(props) {
+  return (
+    <>
+      <Link href={`/blog/${props.id}`} class={`item-box ${props.id}`}>
+        <div class="post-thumb">
+          <div class="post-cats">
+            <span class="post-cat">{props.categoryName}</span>
+          </div>
+          <div class="img-box">
+            <img
+              src={props.thumbnail}
+              alt={props.title}
+              class="fadein-trigger"
+              width="920"
+              height="476"
+            />
+          </div>
+        </div>
+        <div class="post-info">
+          <div class="post-meta">
+            <time datetime={props.publishedAt} class="post-date">
+              {new Date(props.publishedAt)
+                .toLocaleDateString("ja-JP")
+                .split("/")
+                .join(".")}
+            </time>
+          </div>
+          <h3 class="post-ttl">{props.title}</h3>
+        </div>
+      </Link>
+    </>
+  );
+}
+
+function LoopVoice(props) {
+  return (
+    <>
+      <Link class={`item-box ${props.id}`}>
+        <div class="post-thumb">
+          <div class="img-box">
+            <img
+              src={props.thumbnail}
+              alt={props.title}
+              class="fadein-trigger"
+              width="920"
+              height="476"
+            />
+          </div>
+        </div>
+        <div class="post-info">
+          <h3 class="post-ttl">{props.title}</h3>
+        </div>
+      </Link>
+    </>
+  );
+}
+
+export { LoopPost, LoopWorks, LoopEvent, LoopVoice };
